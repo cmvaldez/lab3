40af2a1c7d913698563866549ce3d4792dab44ef
cmvaldez <cmvaldez@student.apc.edu.ph>
2023-02-27 16:47:06 +0800

    Removed double declaration of  in accordance with the table present on my APC website.

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 class NewsModel extends Model {
-    protected $table = 'news';
     protected $table = 'cmvaldez_news';
     public function getNews($slug = false) {
         if ($slug === false) {
