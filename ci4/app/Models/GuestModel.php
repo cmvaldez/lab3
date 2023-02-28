86519becf8ac8b424b4c2352eed80ad4d7e2e6d3
Cheyenne Valdez <cmvaldez@student.apc.edu.ph>
2023-02-28 09:13:46 -0700

    Guest entry, guest form, APC.

---
+++ ci4/app/Models/GuestModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class GuestModel extends Model
+{
+    protected $table = 'cmvaldez_MyGuests';
+
+    protected $allowedFields = ['name', 'email', 'website', 'comment', 'gender'];
+
+    public function getGuest() {
+        return $this->findAll();
+    }
+}
